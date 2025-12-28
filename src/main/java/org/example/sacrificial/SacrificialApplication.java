4b5c32c1b350c138cd52eb3fb68e7daa93808697
Khabir <hakim1khabir2@gmail.com>
2025-12-28 11:09:57 +0100

    init

---
+++ src/main/java/org/example/sacrificial/SacrificialApplication.java
@@ -0,0 +1,13 @@
+package org.example.sacrificial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacrificialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SacrificialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sacrificial/SacrificialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sacrificial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacrificialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
